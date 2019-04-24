29240be3d6d62575ce424904146df708a7541fef
valerieblanchard <vblankwebmaster@gmail.com>
2019-04-24 07:39:18 +0200

    Add Microdata, Role, Aria : First pass.

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,13 +9,13 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="http://schema.org/BlogPosting" role="article">
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark" itemprop="url">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) :
@@ -30,28 +30,39 @@
 	</header><!-- .entry-header -->
 
 	<?php gulpy_post_thumbnail(); ?>
+	
+	<?php // Si on est sur une page de commentaires on n'affiche que le résumé de l'article
+	$cpage = get_query_var( 'cpage' );
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gulpy' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+	if ( is_singular() && $cpage > 0 ): ?>
+		<div class="entry-summary" itemprop="articleExcerpt">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gulpy' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+	<?php
+	else : ?>
+		<div class="entry-content" itemprop="articleBody">
+			<?php
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gulpy' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'gulpy' ),
+				'after'  => '</div>',
+			) );
+			?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
 
 	<footer class="entry-footer">
 		<?php gulpy_entry_footer(); ?>
